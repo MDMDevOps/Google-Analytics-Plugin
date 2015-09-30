8994caf4d3243ddb16526db964a924511d7c7929
Bob Moore <bobjutsu@gmail.com>
2015-09-30 12:12:45 -0500

    s

--- mpress-analytics.php
+++ mpress-analytics.php
@@ -16,7 +16,7 @@
  * Plugin Name: Mpress Google Analytics
  * Plugin URI: http://www.midwestdigitalmarketing.com
  * Description: This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version: 1.0.2
+ * Version: 1.1
  * Author: Midwest Digital Marketing
  * Author URI: http://www.midwestdigitalmarketing.com
  * License: GPL-2.0+
